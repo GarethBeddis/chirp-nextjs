3b2a6df713702c7903ce7bbb864ff463a82a7369
Gareth Beddis <garethbeddis@gmail.com>
2023-03-30 22:23:39 +0100

    Add Post schema / router

---
+++ src/server/api/routers/posts.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const postsRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return ctx.prisma.post.findMany();
+  }),
+});
