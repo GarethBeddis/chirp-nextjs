1a41d2990e391028f056b91c0e7a06fed7d5d0f3
Gareth Beddis <garethbeddis@gmail.com>
2023-04-02 22:31:00 +0100

    Add private TRPC procedure. Add API endpoint for creating posts.

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,7 +3,12 @@
 import { type User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+import z from "zod";
 
 const filterUserForClient = (user: User) => {
   let userFullName = "@";
@@ -47,4 +52,23 @@ export const postsRouter = createTRPCRouter({
       };
     });
   }),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().emoji().min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.user.id;
+
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        },
+      });
+
+      return post;
+    }),
 });
